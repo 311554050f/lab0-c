530840631a2208070d1ba5351d11c882987ba7dd
311554050f <frank123.cs11@nycu.eddu.tw>
2025-04-11 11:09:39 +0800

    Implement core queue operations
    This commit includes the implementation of all basic queue functions:
    - q_new: allocate and initialize queue
    - q_free: release queue memory
    - q_insert_head / q_insert_tail: insert string to queue
    - q_remove_head / q_remove_tail: remove element and copy to buffer
    - q_size: return queue size
    - q_delete_mid: remove middle node in queue
    
    Change-Id: I477210a322415e9553890f98919aeee29ec5300a

--- queue.c
+++ queue.c
@@ -7,46 +7,100 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (head)
+        INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    element_t *entry, *safe;
+    list_for_each_entry_safe(entry, safe, head, list) {
+        list_del(&entry->list);
+        q_release_element(entry);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *element = malloc(sizeof(element_t));
+    if (!element)
+        return false;
+    element->value = strdup(s);
+    if (!element->value) {
+        free(element);
+        return false;
+    }
+    list_add(&element->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    return true;
+    return q_insert_head(head->prev, s);
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *element = list_first_entry(head, element_t, list);
+    list_del(&element->list);
+    if (sp) {
+        strncpy(sp, element->value, bufsize - 1);
+        sp[bufsize - 1] = 0;
+    }
+    return element;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    return q_remove_head(head->prev->prev, sp, bufsize);
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+    int len = 0;
+    struct list_head *li;
+    list_for_each(li, head)
+        len++;
+    return len;
 }
 
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    // list_head *slow = head, *fast = head;
+    // while((fast->next) && (fast->next->next)) {
+
+    // }
+    if (!head || list_empty(head))
+        return false;
+    list_head *slow = head, *fast = head;
+    while ((fast->next != head) && (fast->next->next != head)) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (fast->next != head)
+        slow = slow->next;
+    element_t *element = list_entry(slow, element_t, list);
+    list_del(&element->list);
+    q_release_element(element);
     return true;
 }
 
@@ -54,6 +108,14 @@ bool q_delete_mid(struct list_head *head)
 bool q_delete_dup(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    // if (!head || list_empty(head))
+    //     return false;
+    // element_t *entry, *safe;
+    // list_for_each_entry_safe (entry, safe, head, list) {
+    //     if (strcmp(prev->value, curr->value)) {
+
+    //     }
+    // }
     return true;
 }
 
